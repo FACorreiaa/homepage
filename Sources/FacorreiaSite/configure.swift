4e65be57da10bdf380f5b4f4ecf0606d266b66d0
FACorreiaa <fernandocorreia316@gmail.com>
2026-03-06 18:02:55 +0000

    delete unused comments

--- Sources/FacorreiaSite/configure.swift
+++ Sources/FacorreiaSite/configure.swift
@@ -3,12 +3,7 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // Render HTML for thrown errors instead of Vapor's default JSON payload.
-    app.middleware.use(ErrorPageMiddleware())
-
-    // Serve static files (CSS, JS, images) from /Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
     app.views.use(.leaf)
 
     // register routes
